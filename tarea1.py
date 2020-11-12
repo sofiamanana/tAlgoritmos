a61b7a716b9f4d5b7dae3d31c6a9f83385b1450d
Fernanda Cerda <68760642+nandink@users.noreply.github.com>
2020-11-12 15:41:05 -0300

    Update tarea1.py

--- tarea1.py
+++ tarea1.py
@@ -1,4 +1,3 @@
-#Falta dejar el formato de salida de texto
 listas_wenas=[]
 
 def suma(L,lista,t):
@@ -25,36 +24,34 @@ def suma(L,lista,t):
     return
 
 
+
 #Formato de archivo de texto: T(valor de suma) L(largo de lista) N(L numeros)
-def pasar_a_lista(textfile):
-    t = 0;
-    L = 0;
-    lista_1=[]
-    global listas_wenas
-    archivo = open(textfile, "r")
-    lista = []
-    lista_real = []
-    for linea in archivo:
-        lista = linea.split()
-        t = int(lista[0])
-        L = int(lista[1])
+flag =True
+while flag == True:
+    try:
+        t = 0
+        L = 0
+        lista_1=[]
+        lista_real = []
+        lista = input().strip().split(" ")
+        lista = [int(i) for i in lista]
+        t = lista[0]
+        L = lista[1]
         for i in range(2,L+2):
             lista_real.append(int(lista[i]))
         lista_real.sort()
-        #print(lista_real)
-        print("Suma de",t,":")
-        if (lista_real == []):
-            print("Lista vacia")
-        else:
+        if (lista_real!=[]):
+            print("Suma de",t,":")
             suma(lista_real,lista_1,t)
-        if(listas_wenas==[]):
-            print("NADA")
         #haz algo con la lista_real y volver todo a 0
+        if (listas_wenas==[] and lista_real!=[]):
+            print("NADA")
         t = 0
         L = 0
         listas_wenas=[]
         lista_1 = []
         lista_real = []
-    archivo.close()
-
-pasar_a_lista("algo.txt")
+        
+        
+    except EOFError:
+        flag= False
